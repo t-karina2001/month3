88d438874a7f8f73eb29b3150d2e390c5751cacf
t-karina2001 <134213998+t-karina2001@users.noreply.github.com>
2023-06-23 18:20:29 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from aiogram import executor
 import logging
 from config import dp
@@ -14,3 +15,21 @@ extra.register_extra_handlers(dp)
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     executor.start_polling(dp, skip_updates=True)
+=======
+from aiogram import executor
+import logging
+from config import dp
+from handlers import commands, callbacks, extra, admin
+
+
+commands.register_handlers_commands(dp)
+callbacks.register_handlers_callback(dp)
+admin.register_handler_admins(dp)
+
+extra.register_extra_handlers(dp)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    executor.start_polling(dp, skip_updates=True)
+>>>>>>> git/master
